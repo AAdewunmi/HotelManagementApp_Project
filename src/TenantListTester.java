8c6bf519379d4488ff91660da3fa341be5c626da
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2021-01-24 17:27:44 +0000

    Initial Commit: Payment, PaymentList, PaymentTester, PaymentListTester, Tenant, TenantList, TenantTester, TenantListTester classes and .gitignore file.

---
+++ src/TenantListTester.java
@@ -0,0 +1,13 @@
+/**
+ * TenantListTester Class: Tester class for TenantList class.
+ * @author Adrian Adewunmi
+ * @version 24th January 2021
+ */
+
+public class TenantListTester {
+
+    public static void main(String[] args) {
+
+    }
+
+}
